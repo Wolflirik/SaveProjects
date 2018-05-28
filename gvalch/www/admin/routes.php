b9a003b532850ac250f4ee4b2d0d06713faa7466
Ulvbern <wolflirik@gmail.com>
2018-05-28 21:01:07 +0300

    admin routes

--- gvalch/www/admin/routes.php
+++ gvalch/www/admin/routes.php
@@ -1,3 +1,7 @@
 <?php
 
-$this->router->add('dashboard', '/admin', 'Dashboard:index');
+$this->router->add('dashboard',     '/admin',               'common\Dashboard:index');
+$this->router->add('auth',          '/admin/auth',          'common\Auth:index');
+$this->router->add('auth_login',    '/admin/auth/login',    'common\Auth:login',            'POST');
+$this->router->add('auth_logout',   '/admin/auth/logout',   'common\Auth:logout');
+//$this->router->add('signup', '/admin/auth/signup', 'auth\Signup:index');
